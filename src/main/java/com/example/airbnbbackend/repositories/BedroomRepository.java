2ebb4a9c7d3d3f059826587621196db4c8670b49
hieu <hieutran2811@gmail.com>
2020-08-17 14:44:32 +0700

    xóa file model country , bed, bedroom

--- src/main/java/com/example/airbnbbackend/repositories/BedroomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.airbnbbackend.repositories;
-
-import com.example.airbnbbackend.model.Bedroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface BedroomRepository {
-}
--- src/main/java/com/example/airbnbbackend/repositories/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.airbnbbackend.repositories;
-
-import com.example.airbnbbackend.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface CountryRepository {
-}
